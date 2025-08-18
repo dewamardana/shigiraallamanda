efed8518aea08f50d55853b65769fdeb00b1b258
dewamardana <dodekmardana33@gmail.com>
2025-08-18 08:01:03 +0800

    memperbaiki Load Role

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Role;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,12 +24,14 @@ class AppServiceProvider extends ServiceProvider
     {
 
         // Ambil semua role dari database
-        $roles = Role::all();
+        if (Schema::hasTable('roles')) {
+            $roles = Role::all();
 
-        foreach ($roles as $role) {
-            Gate::define($role->name, function ($user) use ($role) {
-                return $user->roles->contains('name', $role->name);
-            });
+            foreach ($roles as $role) {
+                Gate::define($role->name, function ($user) use ($role) {
+                    return $user->roles->contains('name', $role->name);
+                });
+            }
         }
     }
 }
